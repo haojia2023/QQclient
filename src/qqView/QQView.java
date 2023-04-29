48f110d91ce66da4a62bf94b2980f33ac2c6c0a7
lixun <2574937527@qq.com>
2023-04-29 14:51:30 +0800

    Initial commit

---
+++ src/qqView/QQView.java
@@ -0,0 +1,60 @@
+package qqView;
+
+import java.util.Scanner;
+
+public class QQView {
+    public static void main(String[] args) {
+        new QQView().startMain();
+    }
+    private boolean loop=true;
+    private String[] secMenuOptions = {"显示用户","群发消息","私聊消息","发送文件",null,null,null,null,"系统退出"};
+    private Scanner scanner = new Scanner(System.in);
+    private void startMain(){
+        while(loop){
+            System.out.println("登录界面");
+            System.out.println("1 登录账号");
+            System.out.println("9 退出系统");
+            switch (scanner.nextInt()) {
+                case 1:
+                    System.out.println("请输入账号：");
+                    String name = scanner.next();
+                    System.out.println("请输入密码：");
+                    String psw = scanner.next();
+
+                    if(true) {
+                        System.out.println("登录成功");
+                        int i = 1;
+                        for (String s:secMenuOptions)
+                            if(s != null)
+                                System.out.println(i++ + "\t" + s);
+
+                        switch (scanner.nextInt()){
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                break;
+
+                        }
+                    }else {
+                        System.out.println("登录失败");
+                    }
+                    break;
+                case 9:
+                    loop = false;
+                    break;
+                default:
+                    System.out.println("参数错误");
+                    break;
+
+            }
+
+            }
+        }
+
+    }
